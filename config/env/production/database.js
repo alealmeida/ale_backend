161d23135d1eec9a89d6469e733b31eef1a72aa8
alealmeida <ale.142@icloud.com>
2022-01-24 18:22:26 -0300

    a

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -24,3 +24,26 @@
 //     },
 //   },
 // });
+const parse = require('pg-connection-string').parse;
+const config = parse(process.env.DATABASE_URL);
+
+module.exports = ({ env }) => ({
+  connection: {
+    client: 'postgres',
+    connection: {
+      host: config.host,
+      port: config.port,
+      database: config.database,
+      user: config.user,
+      password: config.password,
+      ssl: {
+        rejectUnauthorized: false
+      },
+    },
+    debug: false,
+  },
+});
+
+// postgres://ebitxebvixeeqd:dc59b16dedb3a1eef84d4999sb4baf@ec2-50-37-231-192.compute-2.amazonaws.com: 5432/d516fp1u21ph7b.
+
+// (This url is read like so: *postgres:// USERNAME : PASSWORD @ HOST : PORT / DATABASE_NAME*)
