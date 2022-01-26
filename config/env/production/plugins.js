3aa445d136ded5a5feaa4be1dea06220e518e1b3
alealmeida <ale.142@icloud.com>
2022-01-26 16:40:12 -0300

    prod

--- config/env/production/plugins.js
+++ config/env/production/plugins.js
@@ -4,11 +4,11 @@ module.exports = {
       config: {
         endpoint: '/graphql',
         shadowCRUD: true,
-        playgroundAlways: false,
+        playgroundAlways: true,
         depthLimit: 7,
         amountLimit: 100,
         apolloServer: {
-          tracing: false,
+          tracing: true,
         },
       },
     },
