d715bc2b8b2a1df449c08404790f1ed43d52140b
alealmeida <ale.142@icloud.com>
2022-01-26 17:48:46 -0300

    dephnolimites

--- config/env/production/plugins.js
+++ config/env/production/plugins.js
@@ -5,7 +5,7 @@ module.exports = {
         endpoint: '/graphql',
         shadowCRUD: true,
         playgroundAlways: true,
-        depthLimit: 7,
+        depthLimit: 30,
         amountLimit: 100,
         apolloServer: {
           tracing: false,
